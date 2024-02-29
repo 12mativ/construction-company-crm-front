092b21fbf49c174eb64dcd1bf177ef0969dc694b
12mativ <ivan_m146@mail.ru>
2024-02-29 21:12:41 +0400

    changed base url

--- http/index.ts
+++ http/index.ts
@@ -1,22 +1,18 @@
-import axios from 'axios';
+import axios from "axios";
 
 const $host = axios.create({
-  baseURL: 'http://localhost:8080'
-})
+  baseURL: "http://178.154.222.147:8000",
+});
 
 const $authHost = axios.create({
   baseURL: 'http://178.154.222.147:8000'
-  // baseURL: 'http://178.154.222.147:8000'
-})
+});
 
 const authInterceptor = (config: any) => {
-  config.headers.authorization = `Bearer ${localStorage.getItem('token')}`
-  return config
-}
+  config.headers.authorization = `Bearer ${localStorage.getItem("token")}`;
+  return config;
+};
 
-$authHost.interceptors.request.use(authInterceptor)
+$authHost.interceptors.request.use(authInterceptor);
 
-export {
-  $host,
-  $authHost
-}
+export { $host, $authHost };
