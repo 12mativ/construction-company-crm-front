e0841c60184b7adc7662795298ef862ef735c2d2
12mativ <ivan_m146@mail.ru>
2024-03-04 23:09:35 +0400

    continue adding orders page

--- components/project/project-orders.tsx
+++ components/project/project-orders.tsx
@@ -5,6 +5,7 @@ import { Table, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { useAppDispatch, useAppSelector } from "@/hooks/redux-hooks";
 import { getOrders } from "@/http/orders/ordersAPI";
 import { OrderType, addOrders } from "@/lib/features/orders/ordersSlice";
+import { cn } from "@/lib/utils";
 import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
 
@@ -27,33 +28,48 @@ const ProjectOrders = () => {
     <div className="flex flex-col gap-y-2 bg-white p-5 rounded-lg shadow-xl">
       <Table>
         <TableHeader>
-          <TableRow key="projectHeader">
-            <TableHead className="w-[18%] px-4">Заказ</TableHead>
-            <TableHead className="flex-1 w-[140px]">Поставщик</TableHead>
-            <TableHead className="w-[140px]">Срок оплаты</TableHead>
-            <TableHead className="w-[140px]">Позиций</TableHead>
-            <TableHead className="w-[140px]">План</TableHead>
-            <TableHead className="w-[140px]">Факт</TableHead>
-            <TableHead className="w-[140px]">Экономия</TableHead>
-            <TableHead className="w-[140px]">Статус</TableHead>
+          <TableRow key="orderHeader">
+            <TableHead className="flex-1 px-4">Заказ</TableHead>
+            <TableHead className="w-[150px] text-center">Поставщик</TableHead>
+            <TableHead className="w-[150px] text-center">Срок оплаты</TableHead>
+            <TableHead className="w-[150px] text-center">Позиций</TableHead>
+            <TableHead className="w-[150px] text-center">План</TableHead>
+            <TableHead className="w-[150px] text-center">Факт</TableHead>
+            <TableHead className="w-[150px] text-center">Экономия</TableHead>
+            <TableHead className="w-[150px] text-center">Статус</TableHead>
           </TableRow>
         </TableHeader>
       </Table>
 
       {orders.map((order) => (
-        <div className="flex">
-          <div className="w-[18%] px-4">1231</div>
-          <div className="w-[140px]">
+        <div className="flex w-full items-center" key={order.id}>
+          <div className="flex-1 px-4 p-2">1231</div>
+          <div className="w-[150px] text-center p-2">
             {
               counterparties.find(
-                (counterparty) => counterparty.id === order.projectId
+                (counterparty) => counterparty.id === order.partnerId
               )?.name
             }
           </div>
-          <div className="w-[140px]">{orders.length}</div>
-          <div className="w-[140px]">==123123</div>
-          <div className="w-[140px]">==123123</div>
-          <div className="w-[140px]">===234234</div>
+          <div className="w-[150px] text-center p-2">Дата</div>
+          <div className="w-[150px] text-center p-2">{orders.length}</div>
+          <div className="w-[150px] text-center p-2">План ₽</div>
+          <div className="w-[150px] text-center p-2">Факт ₽</div>
+          <div className="w-[150px] text-center p-2">Экономия</div>
+          <div
+            className={cn(
+              "w-[150px] text-center p-2 rounded-lg",
+              order.orderType === "NOT_PAID"
+                ? "bg-yellow-200 text-orange-500"
+                : "bg-emerald-200 text-emerald-500"
+            )}
+          >
+            {order.orderType === "NOT_PAID" ? (
+              <p>Ожидается оплата</p>
+            ) : (
+              <p>Оплачено</p>
+            )}
+          </div>
         </div>
       ))}
     </div>
